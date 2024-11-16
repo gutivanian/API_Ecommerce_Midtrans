bbf8bd201d20997269179e7718d29a0d58b362ed
gutivanian <gutivanaliefs@gmail.com>
2024-11-16 21:13:02 +0700

    a

--- routes/payments.js
+++ routes/payments.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getPayments, createPayment, checkTransactionStatus } = require('../controllers/paymentController');
+const { getPayments, createPayment, checkTransactionStatus,handleWebhook } = require('../controllers/paymentController');
 
 router.get('/', getPayments);
 router.post('/', createPayment);
 router.get('/:order_number', checkTransactionStatus);
-router.post('/midtrans-notification', webhookController.handleWebhook);
+router.post('/midtrans-notification', handleWebhook);
 
 module.exports = router;
